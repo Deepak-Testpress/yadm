fe35d5acbe4b9680eb872ee3a8cfe1a006d0ba16
Deepak-Testpress <deepak@testpress.in>
2022-12-21 11:09:12 +0530

    Add sub custom aliases

---
+++ django_alias.sh
@@ -0,0 +1,16 @@
+
+# django alias
+
+alias djapp="python3 manage.py startapp"
+alias djproject="django-admin startproject"
+alias fenv="find . -type f -name activate"
+alias yepenv="source ./env/bin/activate"
+alias nopenv="source ./env/bin/activate"
+
+# python alias
+
+alias pyrun="python3 manage.py runserver"
+alias pipreq="pip freeze > requirements.txt"
+alias pytest="python3 manage.py test"
+alias pymkmig="python3 manage.py makemigrations"
+alias pymig="python3 manage.py migrate"
---
+++ git_alias.sh
@@ -0,0 +1,19 @@
+
+# my git alias
+
+alias gs="git status"
+alias gd="git add"
+alias gc="git commit -m"
+alias gpusho="git push origin HEAD"      # pushes in current branch
+alias gpush="git push origin HEAD"
+alias gpull="git pull origin"
+alias gl="git log"
+alias gll="git log --oneline"
+alias gb="git branch --sort=-committerdate -v"                 # shows branches with verbose
+alias gco="git checkout"
+alias gnb="git checkout -b"
+alias gdb="git branch --delete"
+alias ga="git add"
+alias gaa="git add ."
+alias grb="git rebase"
+alias ic="cp -r ~/pre-commit/. ."
